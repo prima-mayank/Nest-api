bc5b813a623eff2c70637baaa1f0e5a38d09d680
Mayank Shakya <mayank@gmail.com>
2026-02-10 13:27:49 +0530

    middleware added

--- backend/src/middlewares/current-user.middleware.ts
+++ backend/src/middlewares/current-user.middleware.ts
@@ -1,11 +1,36 @@
-
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { JwtPayload, verify } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(`[CurrentUserMiddleware] ${req.method} ${req.originalUrl}`);
+  constructor(private readonly userService:UsersService){}
+  async use(req: Request, _res: Response, next: NextFunction) {
+
+    req['currentUser'] = null;
+    const authHeader = req.headers.authorization;
+    if (!authHeader || typeof authHeader !== 'string' || !authHeader.startsWith('Bearer ')) {
+      return next();
+    }
+
+    const token = authHeader.split(' ')[1];
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (!token || !jwtSecret) {
+      return next();
+    }
+
+    try {
+      const payload =<JwtPayload> verify(token, jwtSecret);
+      const {id,...rest} = payload
+
+
+      const currentuser =await this.userService.findOne(+id)
+    } catch (err) {
+      console.error('Error verifying JWT token:', err);
+    }
+
     next();
   }
 }
