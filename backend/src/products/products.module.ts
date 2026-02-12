99f7618742ba1d10ce83f6280303061be6d97ec0
Mayank Shakya <mayank@gmail.com>
2026-02-12 11:32:20 +0530

    create product route

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -3,8 +3,10 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
+import { CatagoriesModule } from 'src/catagories/catagories.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+
+  imports: [TypeOrmModule.forFeature([ProductEntity]), CatagoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
